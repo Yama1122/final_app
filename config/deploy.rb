0667f13c5a4f749720f0b86f6d1d86b1bc9353f8
Yama1122 <yamashita1122.you@gmail.com>
2020-08-16 15:06:37 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "3.14.1"
 
-set :application, "final_app"
-set :repo_url, "git@github.com:Yama1122/final_app.git"
+set :application, 'final_app'
+set :repo_url, 'git@github.com:Yama1122/final_app.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
