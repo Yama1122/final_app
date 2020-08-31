075c4af68331b817e494548f1a6fb145d82237ff
Yama1122 <yamashita1122.you@gmail.com>
2020-08-31 23:13:47 +0900

    resolve_Conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'products#index'
+  root :to 'products#index'
   resources :products
   resources :users
   resources :credit_cards
+  resources 'searches', only: :index
 end
