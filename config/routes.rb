28d68d127158659ab60f4cbbc1e19cbef8d4d3a6
Yama1122 <yamashita1122.you@gmail.com>
2020-09-01 22:56:26 +0900

    logout_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to:'products#index'
   resources :products
-  resources :users
+  resources :users do
+    member do
+      get 'logout'
+    end
+  end
   resources :credit_cards
   resources 'searches', only: :index
 end
