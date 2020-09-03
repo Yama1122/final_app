10ebc18d7ba17bf27f1e0b1befe207bee3c1841a
Yama1122 <yamashita1122.you@gmail.com>
2020-09-03 20:21:02 +0900

    routes_change

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'products#index'
-  resources :products
-  resources :users do
+  resources :products, only: [:index,:show,:new]
+  resources :users, only: :show do
     member do
       get 'logout'
     end
   end
-  resources :credit_cards
+  resources :credit_cards, only: :new
   resources 'searches', only: :index
 end
