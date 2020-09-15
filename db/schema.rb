4872f858bb5a15479d3ae4e444a9f33defd826fd
tama555 <yoshi4548797@yahoo.co.jp>
2020-09-15 22:39:53 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,18 @@ ActiveRecord::Schema.define(version: 2020_09_10_130022) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", null: false
+    t.string "password", null: false
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.date "birth_year", null: false
+    t.date "birth_month", null: false
+    t.date "birth_day", null: false
+    t.string "image"
+    t.text "introduction"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
