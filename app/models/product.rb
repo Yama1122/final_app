d57bce9620e9bd9da5857d80c3809a291d5a66be
Yama1122 <yamashita1122.you@gmail.com>
2020-09-16 15:34:49 +0900

    出品ページ発送元地域ボックスの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,14 @@ class Product < ApplicationRecord
   has_many :users, dependent: :destroy
   belongs_to :brand
   belongs_to :category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :size
   belongs_to_active_hash :condition
   belongs_to_active_hash :sendingday
   belongs_to_active_hash :postage
   belongs_to_active_hash :sendingtype
+  belongs_to_active_hash :prefecture_code
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
 end
