f48bedac1bcad4d80a79eeb23e3d99014aa73e7f
Yama1122 <yamashita1122.you@gmail.com>
2020-09-16 16:20:13 +0900

    商品詳細ページにおけるカテゴリーの表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < ApplicationController
-
+  before_action :set_product, only: :show
   def index 
   end
 
+  def show
+    @category = @product.category
+  end
+
 end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
