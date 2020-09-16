6ced97db8df13e1fc57d5e2ba07b0563d90c3e4c
Yama1122 <yamashita1122.you@gmail.com>
2020-09-16 14:51:48 +0900

    商品出品ページのカテゴリ機能

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,4 +1,6 @@
 $(function(){
+// ---topページここから---
+
   // カテゴリアイコンをホバーするとメニュ-を表示
   $(".listsleft__item--first").hover(function(e){  
     e.preventDefault();
@@ -97,4 +99,117 @@ $(function(){
     mouseenter: function () {
     }
   }, '#tree_menu');
+// ---topページここまで---
+
+
+// ---商品出品ページここから---
+
+  // カテゴリ選択ボックス内のoptionを追加
+  function buildOption(child){
+    let html = `<option value="${child.id}">${child.name}</option>`;
+    return html;
+  }
+
+  // 子カテゴリの選択ボックスを追加
+  function buildChild(insertHTML) {
+    let html = `<div class="category__list_box--child">
+                  <select class="text" id="child-form" name="child">
+                    <option value="">---</option>  //初期値の設定
+                    ${insertHTML}
+                  </select>
+                </div>
+              `
+    return html;
+  }
+
+  // 孫カテゴリの選択ボックスを追加
+  function buildGrandChild(insertHTML) {
+    var html = `
+                <div class="category__list_box--gc">
+                  <select class="text" id="grandchild-form" name="product[category_id]">
+                    <option value="">---</option>
+                    ${insertHTML}
+                  </select>
+                </div>
+              `
+    return html;
+  }
+
+  // 親カテゴリを選択すると子カテゴリを表示
+  $("#parent-form").on("change", function(){
+    let parentValue = document.getElementById("parent-form").value;
+    if (parentValue != '') {  //選択した親要素が初期値以外なら動作
+      $.ajax({
+        url: '/categories/search',
+        type: "GET",
+        data: {
+          parent_id: parentValue
+        },
+        dataType: 'json'
+      })
+      .done(function(data){
+        let insertHTML = '';
+        data.forEach(function(child){
+          insertHTML += buildOption(child);
+        });
+        let html = buildChild(insertHTML);
+        $('.category__list_box').append(html);
+        $("#parent-form").on("change", function(){
+          $(".category__list_box--child").remove();  //親カテゴリ選択時に子・孫カテゴリをリセット
+          $(".category__list_box--gc").remove();
+          $(".brand").remove();
+        });
+
+        // 子カテゴリを選択すると孫カテゴリを表示
+        $("#child-form").on("change", function(){
+          console.log("hello");
+          let childrenValue = document.getElementById("child-form").value;
+          if (childrenValue != '') {
+            $.ajax({
+              url: '/categories/search',
+              type: "GET",
+              data: {
+                children_id: childrenValue
+              },
+              dataType: 'json'
+            })
+            .done(function(data){
+              // 孫カテゴリのデータがある・ないで分岐
+              let length = data.length
+              // 孫カテゴリがない場合ブランドを追加
+              if (length == 0) {
+                $('.brand').css("display", "block");
+              // 孫カテゴリがある場合
+              } else {
+                let insertHTML = '';
+                data.forEach(function(grandchild){
+                  insertHTML += buildOption(grandchild);
+                });
+                let html = buildGrandChild(insertHTML);
+                $('.category__list_box').append(html);
+                // 孫カテゴリ選択でブランドが表示
+                $("#grandchild-form").on("change", function(){
+                  $('.brand').css("display", "block");
+                });
+              }
+              $("#child-form").on("change", function(){
+                $(".category__list_box--gc").remove();  //再度,子カテゴリ選択で孫カテゴリリセット
+                $(".brand").hide();
+              });
+            })
+            .fail(function(){
+              alert('カテゴリー取得に失敗しました');
+            })
+          }
+        });
+      })
+      .fail(function(){
+        alert('カテゴリー取得に失敗しました');
+      })
+    }
+  });
+
+// ---商品出品ページここまで---
+
+
 });
