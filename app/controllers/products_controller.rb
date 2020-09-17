2afeacc7d3b60ba5245ba5f63e114f2138ca3f00
Kazu-03 <66908956+Kazu-03@users.noreply.github.com>
2020-09-17 22:10:51 +0900

    商品出品機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,34 @@
 class ProductsController < ApplicationController
   def index
-    @products =Product.all
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
+    @product.product_images.new
+  end
+
+  def create
+    @products = Product.new(product_params)
+    if @products.save!
+      redirect_to root_path
+    else
+      redirect_to new_product_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+private
+  def product_params
+    params.require(:product).permit(:name, :price, :category, :condition, :sendingday, :brand, :postage, :sendingtype, product_images_attributes: [:url])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'products#index'
-  resources :products, only: [:index,:show,:new] do
-    member do
-      get 'confirmation'
-    end
+    resources :products, only: [:index,:show,:new, :create] do
+      member do
+        get 'confirmation'
+      end
   end
   
   resources :users, only: :show do
@@ -15,3 +15,4 @@ Rails.application.routes.draw do
   resources :credit_cards, only: :new
   resources 'searches', only: :index
 end
+
