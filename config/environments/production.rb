7e24a72bdf9ac5573d1f55bf01a65b02f3b83669
Yama1122 <yamashita1122.you@gmail.com>
2020-09-17 20:25:21 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.serve_static_assets = false
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier.new(harmony: true)
+  # config.assets.js_compressor = :uglifier.new(harmony: true)
   config.assets.compress = true
   # config.assets.css_compressor = :sass
 
