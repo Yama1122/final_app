0152c03a509c32124d2b3d737a8ab61a24594003
tama555 <yoshi4548797@yahoo.co.jp>
2020-09-27 17:05:57 +0900

    ユーザー登録機能導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe '#create' do
+    it "nicknameがない場合は登録できないこと" do
+      user = build(:user, nickname: "")
+      # factory_botを使用
+      user.valid?
+      expect(user.errors[:nickname]).to include("を入力してください")
+      # 当アプリは日本語版へ変更しているためinclude("can't be blank")にするとエラーが生じる。
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "passwordが存在してもpassword_confirmationがない場合は登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
+    it " passwordが6文字以下であれば登録できないこと " do
+      user = build(:user, password: "000000", password_confirmation: "000000")
+      user.valid?
+      expect(user.errors[:password]).to include("は7文字以上で入力してください")
+    end
+
+    it " passwordが7文字以上であれば登録できること " do
+      user = build(:user, password: "1234567", password_confirmation: "1234567")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    it "必須項目が埋められていれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
 end
