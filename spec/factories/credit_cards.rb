e2f22c7baa8aac82468b92a9d342fd3773d0e80e
Yama1122 <yamashita1122.you@gmail.com>
2020-09-29 21:49:24 +0900

    単体テストの実施

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :credit_card do
-    
+    id{1}
+    customer_id{"cus_xxxxxxxxxxxxxx"}
+    card_id{"car_yyyyyyyyyyyyyyy"} 
+    association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+    id{"1"}
+    email{"test@user"}
+    encrypted_password{"XXXXXX"}
+    created_at{"2020-05-22 00:00:00"}
+    updated_at{"2020-05-22 00:00:00"}
+    nickname{"test"}
+    first_name{"テスト"}
+    family_name{"テスト"}
+    first_name_kana{"テスト"}
+    family_name_kana{"テスト"}
+    birth_date{"2020-08-16"}
+    introduction{"テスト"}
   end
 end
