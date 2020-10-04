1282c6acf82f5853e0d6d42d3c67ee7034361c79
Yama1122 <yamashita1122.you@gmail.com>
2020-10-04 23:18:01 +0900

    エラー解消

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   
   before_action :set_product, only: [:show,:destroy,:delete]
-  before_action :set_product_image, except: :index
+  before_action :set_product_image, except: [:index,:new,:create]
   
  
   def index
@@ -49,20 +49,13 @@ class ProductsController < ApplicationController
   def show
     @category = @product.category
   end
-private
-  def product_params
-    params.require(:product).permit(:name, :profile, :price, :category_id, :condition_id, :sendingday_id, :prefecture_code_id, :postage_id, :sendingtype_id, product_images_attributes: [:url, :_destroy, :id[0]]).merge(seller_id: current_user.id)
-  end
-
-  def show
-    @category = @product.category
-  end
 
 
   private
   def product_params
-    params.require(:product).permit(:name,:profile,:price,:size,:condition_id,:sendingday_id,:postage_id,:prefecture_code_id,:sendingtype_id,:deal_closed_day).merge(user_id:current_user.id,category_id:params[:product][:category_id],brand_id:params[:products][:brand_id],seller_id:params[:products][:seller_id],buyer_id:params[:product][:buyer_id])
+    params.require(:product).permit(:name, :profile, :price, :category_id, :condition_id, :sendingday_id, :prefecture_code_id, :postage_id, :sendingtype_id, product_images_attributes: [:url, :_destroy, :id[0]]).merge(seller_id: current_user.id)
   end
+  
   def set_product
     @product = Product.find(params[:id])
   end
