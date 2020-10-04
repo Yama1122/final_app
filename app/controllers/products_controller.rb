7665095053111357add366fc44c3f24cec5a29b6
Kazu-03 <66908956+Kazu-03@users.noreply.github.com>
2020-10-04 21:57:47 +0900

    model修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   
-  before_action :set_product, only: [:show,:destroy,:delete]
-  before_action :set_product_image, except: :index
+  before_action :set_product, only: [:index, :new,:show,:destroy,:delete]
+  before_action :set_product_image, except: [:index, :new]
   
  
   def index
