5b1b3c2d3f0e095500cdd8069da741148e5fe35d
Yama1122 <yamashita1122.you@gmail.com>
2020-10-10 12:51:30 +0900

    商品詳細ページのパンくずリスト実装

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -8,6 +8,70 @@ crumb :user do
   link "マイページ",user_path(current_user.id)
 end
 
+# ログアウトページ
+crumb :logout do
+  link "ログアウト",logout_user_path(current_user.id)
+  parent :user
+end
+
+
+# クレジット新規登録
+crumb :credit_new do
+  link "クレジットカード新規登録",new_credit_card_path
+  parent :user
+end
+
+# クレジット一覧
+crumb :credit_index do
+  link "登録クレジットカード情報",credit_card_path(params[:id])
+  parent :user
+end
+
+# 商品詳細ページ
+crumb :product do |product|
+  product = Product.find(params[:id])
+  category = Category.find(product.category_id)
+  link "#{product.name}",product_path(product)
+  if category.has_children?
+    parent :product_child_category
+  else
+    parent :product_grandchild_category
+  end
+end
+
+# 商品詳細親カテゴリ
+crumb :product_parent_category do |product|
+  product = Product.find(params[:id])
+  category = Category.find(product.category_id).root
+  link "#{category.name}",category_path(category)
+  parent :category_index
+end
+
+# 商品詳細子カテゴリ
+crumb :product_child_category do |product|
+  product = Product.find(params[:id])
+  category = Category.find(product.category_id)
+  # 表示ページが子カテゴリ一（子供がいる）場合
+  if category.has_children?
+    link "#{category.name}", category_path(category)
+    parent :product_parent_category
+    # 表示ページが孫カテゴリーの場合
+  else
+    link "#{category.parent.name}", category_path(category.parent)
+    parent :product_parent_category
+  end
+end
+
+# 商品詳細孫カテゴリ
+crumb :product_grandchild_category do |product|
+  product = Product.find(params[:id])
+  category = Category.find(product.category_id)
+  link "#{category.name}", category_path(category)
+  parent :product_child_category
+end
+
+
+
 # カテゴリ一覧
 crumb :category_index do
   link "カテゴリー一覧", categories_path
