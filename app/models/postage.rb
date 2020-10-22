ba639152909cd56de2e41a00ee9a633b47179b6d
Kazu-03 <66908956+Kazu-03@users.noreply.github.com>
2020-10-23 01:59:08 +0900

    バリデーション修正

--- app/models/postage.rb
+++ app/models/postage.rb
@@ -1,6 +1,6 @@
 class Postage < ActiveHash::Base
   self.data = [
-      {id: 0, name: '選択してください'},
-      {id: 1, name: '送料込み（出品者負担）'}, {id: 2, name: '送料別（購入者負担）'},  
+      # {id: 0, name: '選択してください'}を削除しました
+      {id: 0, name: '送料込み（出品者負担）'}, {id: 1, name: '送料別（購入者負担）'},  
   ]
 end
--- app/models/sendingtype.rb
+++ app/models/sendingtype.rb
@@ -1,9 +1,9 @@
 class Sendingtype < ActiveHash::Base
   self.data = [
-      {id: 0, name: '選択してください'},
-      {id: 1, name: '未定'}, {id: 2, name: 'らくらくメルカリ便'}, {id: 3, name: 'ゆうゆうメルカリ便'},
-      {id: 4, name: 'ゆうメール'}, {id: 5, name: 'レターパック'}, {id: 6, name: '普通郵便（定型、定型'}, 
-      {id: 7, name: 'クロネコヤマト'}, {id: 8, name: 'ゆうパック'}, {id: 9, name: 'クリックポスト'}, 
-      {id: 10, name: 'ゆうパケット'},  
+      # {id: 0, name: '選択してください'}を削除しました,
+      {id: 0, name: '未定'}, {id: 1, name: 'らくらくメルカリ便'}, {id: 2, name: 'ゆうゆうメルカリ便'},
+      {id: 3, name: 'ゆうメール'}, {id: 4, name: 'レターパック'}, {id: 5, name: '普通郵便（定型、定型'}, 
+      {id: 6, name: 'クロネコヤマト'}, {id: 7, name: 'ゆうパック'}, {id: 8, name: 'クリックポスト'}, 
+      {id: 9, name: 'ゆうパケット'},  
   ]
 end
