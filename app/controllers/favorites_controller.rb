0e0fd30cdf7f622645dbccb194a255ff3c57ea1c
tama555 <yoshi4548797@yahoo.co.jp>
2020-10-22 14:05:10 +0900

    set_product_plus

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,9 @@
 class FavoritesController < ApplicationController
+  before_action :set_product, only: %i[create destroy]
+
+
   def create
     user=current_user
-    @product=Product.find(params[:product_id])
     if Favorite.create(user_id: user.id,product_id:@product.id)
 
     else
@@ -11,12 +13,17 @@ class FavoritesController < ApplicationController
 
   def destroy
     user=current_user
-    @product=Product.find(params[:product_id])
     if favorite=Favorite.find_by(user_id: user.id,product_id:@product.id)
       favorite.delete
     else
       redirect_to root_url
     end
   end
+
+  private
+  def set_product
+    @product=Product.find(params[:product_id])
+  end
+
 end
 
