9f53005efac3087ab6186d27543b2bd526ef054b
tama555 <yoshi4548797@yahoo.co.jp>
2020-10-30 21:56:07 +0900

    huruma_chenged

--- app/models/sendingtype.rb
+++ app/models/sendingtype.rb
@@ -1,7 +1,7 @@
 class Sendingtype < ActiveHash::Base
   self.data = [
       # {id: 0, name: '選択してください'}を削除しました,
-      {id: 0, name: '未定'}, {id: 1, name: 'らくらくメルカリ便'}, {id: 2, name: 'ゆうゆうメルカリ便'},
+      {id: 0, name: '未定'}, {id: 1, name: 'らくらくフリマ便'}, {id: 2, name: 'ゆうゆうフリマ便'},
       {id: 3, name: 'ゆうメール'}, {id: 4, name: 'レターパック'}, {id: 5, name: '普通郵便（定型、定型'}, 
       {id: 6, name: 'クロネコヤマト'}, {id: 7, name: 'ゆうパック'}, {id: 8, name: 'クリックポスト'}, 
       {id: 9, name: 'ゆうパケット'},  
